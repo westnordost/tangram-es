dfb6cc0e7556af7acb34a4fefd743b8931fe7279
Matt Blair <blair1618@gmail.com>
2018-06-20 18:31:57 -0400

    Reconfiguring the iOS build setup more
    Now we can build the app and framework in different configurations from a single workspace!
    However it requires a workspace setting to use the build paths configured in each target, instead of overriding the build paths with a shared workspace build folder (the default behavior).

--- platforms/ios/config.cmake
+++ platforms/ios/config.cmake
@@ -2,85 +2,79 @@ add_definitions(-DTANGRAM_IOS)
 
 set(TANGRAM_FRAMEWORK_VERSION "0.9.4-dev")
 
-### Emulate toolchain --
+### Configure iOS toolchain.
 set(IOS TRUE)
 set(CMAKE_OSX_SYSROOT "iphoneos")
 set(CMAKE_XCODE_EFFECTIVE_PLATFORMS "-iphoneos;-iphonesimulator")
 set(CMAKE_XCODE_ATTRIBUTE_IPHONEOS_DEPLOYMENT_TARGET "9.3")
-# set(CMAKE_XCODE_ATTRIBUTE_ONLY_ACTIVE_ARCH[variant=debug] "YES")
-# set(CMAKE_XCODE_ATTRIBUTE_ONLY_ACTIVE_ARCH[variant=release] "NO")
-### -- end
 
 # Tell SQLiteCpp to not build its own copy of SQLite, we will use the system library instead.
 set(SQLITECPP_INTERNAL_SQLITE OFF CACHE BOOL "")
 
 set(TANGRAM_FRAMEWORK_HEADERS
-    platforms/ios/Framework/TangramMap.h
-    platforms/ios/TangramMap/TGGeoPolyline.h
-    platforms/ios/TangramMap/TGGeoPolygon.h
-    platforms/ios/TangramMap/TGGeoPoint.h
-    platforms/ios/TangramMap/TGMarker.h
-    platforms/ios/TangramMap/TGSceneUpdate.h
-    platforms/ios/TangramMap/TGMapData.h
-    platforms/ios/TangramMap/TGTypes.h
-    platforms/ios/TangramMap/TGHttpHandler.h
-    platforms/ios/TangramMap/TGLabelPickResult.h
-    platforms/ios/TangramMap/TGMarkerPickResult.h
-    platforms/ios/TangramMap/TGMapViewController.h
+  platforms/ios/framework/src/TangramMap.h
+  platforms/ios/framework/src/TGGeoPolyline.h
+  platforms/ios/framework/src/TGGeoPolygon.h
+  platforms/ios/framework/src/TGGeoPoint.h
+  platforms/ios/framework/src/TGMarker.h
+  platforms/ios/framework/src/TGSceneUpdate.h
+  platforms/ios/framework/src/TGMapData.h
+  platforms/ios/framework/src/TGTypes.h
+  platforms/ios/framework/src/TGHttpHandler.h
+  platforms/ios/framework/src/TGLabelPickResult.h
+  platforms/ios/framework/src/TGMarkerPickResult.h
+  platforms/ios/framework/src/TGMapViewController.h
 )
 
 add_library(TangramMap SHARED
-    ${TANGRAM_FRAMEWORK_HEADERS}
-    platforms/common/platform_gl.cpp
-    platforms/common/appleAllowedFonts.h
-    platforms/common/appleAllowedFonts.mm
-    platforms/ios/TangramMap/iosPlatform.h
-    platforms/ios/TangramMap/iosPlatform.mm
-    platforms/ios/TangramMap/TGHelpers.h
-    platforms/ios/TangramMap/TGHelpers.mm
-    platforms/ios/TangramMap/TGGeoPolyline.mm
-    platforms/ios/TangramMap/TGGeoPolygon.mm
-    platforms/ios/TangramMap/TGHttpHandler.mm
-    platforms/ios/TangramMap/TGMapData+Internal.h
-    platforms/ios/TangramMap/TGMapData.mm
-    platforms/ios/TangramMap/TGSceneUpdate.mm
-    platforms/ios/TangramMap/TGLabelPickResult+Internal.h
-    platforms/ios/TangramMap/TGLabelPickResult.mm
-    platforms/ios/TangramMap/TGMarkerPickResult+Internal.h
-    platforms/ios/TangramMap/TGMarkerPickResult.mm
-    platforms/ios/TangramMap/TGMarker+Internal.h
-    platforms/ios/TangramMap/TGMarker.mm
-    platforms/ios/TangramMap/TGTypes.mm
-    platforms/ios/TangramMap/TGMapViewController+Internal.h
-    platforms/ios/TangramMap/TGMapViewController.mm
+  ${TANGRAM_FRAMEWORK_HEADERS}
+  platforms/common/platform_gl.cpp
+  platforms/common/appleAllowedFonts.h
+  platforms/common/appleAllowedFonts.mm
+  platforms/ios/framework/src/iosPlatform.h
+  platforms/ios/framework/src/iosPlatform.mm
+  platforms/ios/framework/src/TGHelpers.h
+  platforms/ios/framework/src/TGHelpers.mm
+  platforms/ios/framework/src/TGGeoPolyline.mm
+  platforms/ios/framework/src/TGGeoPolygon.mm
+  platforms/ios/framework/src/TGHttpHandler.mm
+  platforms/ios/framework/src/TGMapData+Internal.h
+  platforms/ios/framework/src/TGMapData.mm
+  platforms/ios/framework/src/TGSceneUpdate.mm
+  platforms/ios/framework/src/TGLabelPickResult+Internal.h
+  platforms/ios/framework/src/TGLabelPickResult.mm
+  platforms/ios/framework/src/TGMarkerPickResult+Internal.h
+  platforms/ios/framework/src/TGMarkerPickResult.mm
+  platforms/ios/framework/src/TGMarker+Internal.h
+  platforms/ios/framework/src/TGMarker.mm
+  platforms/ios/framework/src/TGTypes.mm
+  platforms/ios/framework/src/TGMapViewController+Internal.h
+  platforms/ios/framework/src/TGMapViewController.mm
 )
 
 target_link_libraries(TangramMap PRIVATE
-    tangram-core
-    sqlite3
-    # Frameworks: use quotes so "-framework X" is treated as a single linker flag.
-    "-framework CoreFoundation"
-    "-framework CoreGraphics"
-    "-framework CoreText"
-    "-framework GLKit"
-    "-framework OpenGLES"
-    "-framework UIKit"
+  tangram-core
+  sqlite3
+  # Frameworks: use quotes so "-framework X" is treated as a single linker flag.
+  "-framework CoreFoundation"
+  "-framework CoreGraphics"
+  "-framework CoreText"
+  "-framework GLKit"
+  "-framework OpenGLES"
+  "-framework UIKit"
 )
 
 target_include_directories(TangramMap PRIVATE
-    platforms/common
+  platforms/common
 )
 
 set_target_properties(TangramMap PROPERTIES
-    FRAMEWORK TRUE
-    PUBLIC_HEADER "${TANGRAM_FRAMEWORK_HEADERS}"
-    MACOSX_FRAMEWORK_INFO_PLIST "${PROJECT_SOURCE_DIR}/platforms/ios/framework/Info.plist"
-    XCODE_ATTRIBUTE_CURRENT_PROJECT_VERSION "${TANGRAM_FRAMEWORK_VERSION}"
-    XCODE_ATTRIBUTE_DEFINES_MODULE "YES"
-    XCODE_ATTRIBUTE_CLANG_ENABLE_OBJC_ARC "YES"
-    XCODE_ATTRIBUTE_CLANG_CXX_LANGUAGE_STANDARD "c++14"
-    XCODE_ATTRIBUTE_CLANG_CXX_LIBRARY "libc++"
+  FRAMEWORK TRUE
+  PUBLIC_HEADER "${TANGRAM_FRAMEWORK_HEADERS}"
+  MACOSX_FRAMEWORK_INFO_PLIST "${PROJECT_SOURCE_DIR}/platforms/ios/framework/Info.plist"
+  XCODE_ATTRIBUTE_CURRENT_PROJECT_VERSION "${TANGRAM_FRAMEWORK_VERSION}"
+  XCODE_ATTRIBUTE_DEFINES_MODULE "YES"
+  XCODE_ATTRIBUTE_CLANG_ENABLE_OBJC_ARC "YES"
+  XCODE_ATTRIBUTE_CLANG_CXX_LANGUAGE_STANDARD "c++14"
+  XCODE_ATTRIBUTE_CLANG_CXX_LIBRARY "libc++"
 )
-
-# Set RPATH to be within the application /Frameworks directory
-# set_xcode_property(TangramMap LD_DYLIB_INSTALL_NAME "@rpath/${FRAMEWORK_NAME}.framework/${FRAMEWORK_NAME}")
